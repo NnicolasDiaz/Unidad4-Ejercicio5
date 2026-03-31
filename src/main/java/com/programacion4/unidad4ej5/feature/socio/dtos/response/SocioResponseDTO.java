15ee8c28ef8f337beafd6fe2cef919bab7c7f24b
NnicolasDiaz <nicoprodiaz@gmail.com>
2026-03-31 10:17:16 -0300

    Completo Ejercicio 5: DTOs y validaciones US01 a US04

---
+++ src/main/java/com/programacion4/unidad4ej5/feature/socio/dtos/response/SocioResponseDTO.java
@@ -0,0 +1,53 @@
+package com.programacion4.unidad4ej5.feature.socio.dtos.response;
+//US01
+
+public class SocioResponseDTO {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String dni;
+
+    // --- GETTERS Y SETTERS ---
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
